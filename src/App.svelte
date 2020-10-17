5046eeda47e3e748c02eaaafa8aa3750ac93db17
unknown <brendenferguson@gmail.com>
2020-10-16 17:19:44 -0700

    added payment calculation

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import {fade} from 'svelte/transition'
-
-
-
     let userInput: string;
     let paid: string;
 
@@ -19,20 +15,45 @@
     let items: item[] = [];
     let users: user[] = [];
 
-    const onEnter = (e): void => {
+    const onEnter = (e: KeyboardEvent): void => {
         if (e.key === "Enter") {
             addUser(userInput);
             addItem(userInput);
         }
     };
 
-    const validityCheck = (input:string): boolean => {
+    const validityCheck = (input: string): boolean => {
         let splitIn: string[] = input.split(" ");
         if (splitIn[0].length === 1 && Number(splitIn[2])) {
             return true;
         }
 
         return false;
+    };
+
+    const addUser = (input: string): void => {
+        if (validityCheck(input)) {
+            let user: user = {
+                name: input[0],
+                total: 0,
+            };
+
+            let userFound = false;
+            for (const each of users) {
+                if (each.name === user.name) {
+                    userFound = true;
+                }
+            }
+            if (!userFound) users = [...users, user];
+        }
+    };
+
+    const updateUser = (entry: item): void => {
+        for (const ea of users) {
+            if (entry.user === ea.name) {
+                ea.total += entry.price;
+            }
+        }
     };
 
     const addItem = (input: string): void => {
@@ -45,57 +66,28 @@
                 price: Number(splitInput[2]),
             };
 
-			items = [...items, entry];
-			
-	
-		
-			updateUser(entry)
+            items = [...items, entry];
 
-
+            updateUser(entry);
 
             userInput = "";
             document.getElementById("userinput").focus();
         } else {
             userInput = "";
         }
-	};
-	
-	const updateUser = (entry:item):void=>{
-		for (const ea of users){
-				if (entry.user === ea.name){
-					ea.total+=entry.price
-				}
-			}
-			
-			
-	}
+    };
 
-    const addUser = (input: string): void => {
-        if (validityCheck(input)) {
-
-            let user: user = {
-                name: input[0],
-                total: 0,
-			};
-			
-			let userFound = false
-			for (const each of users){
-				if (each.name === user.name){
-					userFound =true
-				}
-			}
-			if (! userFound ) users = [...users,user]
-			
-
-
-
+    $: shared = users.filter((user) => user.name === "s");
+    $: numUsers = shared.length > 0 ? users.length - 1 : 1;
+    const owed = (user) => {
+        if (user.name !== "s" && user.name !== paid) {
+            return `| Pay ${(
+                user.total +
+                (shared.length > 0 ? shared[0].total / numUsers : 0)
+            ).toFixed(2)}`;
         }
-        
-	};
-	
-
-
-
+        return "";
+    };
 </script>
 
 <style>
@@ -125,24 +117,28 @@
     <p>
         {#if paid && items.length > 0}
             {#key items}
-				<h3>Totals</h3>
-				{#each users as user}
-					 <div in:fade>
-						{user.name} :
-						${user.total.toFixed(2)} 
-					 </div>
-				{/each}
-					
+                <h3>Totals</h3>
+                {#each users as user}
+                    <div>
+                        {user.name === 's' ? 'Shared' : user.name}
+                        : ${user.total.toFixed(2)}
+                        {owed(user)}
+                    </div>
+                {/each}
 
-				<h3>Items</h3>
-					 <ul>
-
-				{#each items as item}
-					<li>{item.user === 's' ? 'Everyone' : item.user} bought {item.name} for ${item.price.toFixed(2)}</li>
-				{/each}
-			</ul>
-		
+                <h3>Items</h3>
+                <ul>
+                    {#each items as item}
+                        <li>
+                            {item.user === 's' ? 'Everyone' : item.user}
+                            bought
+                            {item.name}
+                            for ${item.price.toFixed(2)}
+                        </li>
+                    {/each}
+                </ul>
             {/key}
         {:else}Waiting for items and who paid.{/if}
     </p>
+    <button on:click={() => location.reload()}>Reset</button>
 </main>
